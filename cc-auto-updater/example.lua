ec654c3a9583b8585cc1764a1c727cdcd43508ab
Kolpa <kolyaopahle@gmail.com>
2013-01-02 14:55:22 +0100

    updated documentation

--- cc-auto-updater/example.lua
+++ cc-auto-updater/example.lua
@@ -4,4 +4,4 @@ local function success()
 	print("done")
 end
 
-updater.update(success, "Kolpa", "lol-pro-spectator")
+updater.update(success, "Kolpa", "lol-pro-spectator") -- first argument is the function to be executed in case of an update second is the username and last is the repostery
